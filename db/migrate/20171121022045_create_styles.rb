40bc4df6885bd070e471e6e11e096a54f5372f9c
Clement Picard-Zivy <clementpicardzivy@gmail.com>
2017-11-20 21:24:00 -0500

    first commit model schema ok

---
+++ db/migrate/20171121022045_create_styles.rb
@@ -0,0 +1,11 @@
+class CreateStyles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :styles do |t|
+      t.string :name
+      t.string :cover
+      t.integer :year
+
+      t.timestamps
+    end
+  end
+end
